d3622cc541d4dfe40cd9c1bdda8b48d9c17dca0f
madjake45 <madjake45@madjake45.me>
2014-10-15 01:58:57 +0100

    Initial Commit from original Repo
    Please reference the dev repo for all the old commits

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" href="http://upload.wikimedia.org/wikipedia/commons/d/d7/Sad-pug.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="Style.css" type="text/css">
+        <link rel="stylesheet" href="bootstrap.min.css" type="text/css">
+        <script src="Disco.js"></script>
+        <script src="jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+        <script src="bootstrap.min.js"></script>
+        <script src="Image.js"></script>
+        <meta name="author" content="madjake45" />
+        <title>PUGS!!!</title>
+        
+    </head>
+<body onload="disco(); firstimage();">
+<audio autoplay loop src="Rick.mp3"></audio>
+<div id="body">
+    <H1 align="Center">ATTACK OF THE PUGS!!!</H1>
+
+    <div id="pug">
+        <img id="pugimage" src="http://madjake45.com/turtle.png" alt="PUGS!!!"/>
+    </div>
+</div>
+    </body>
+<footer onload="image()">
+    <br>
+    <a id="madjake45" href="http://madjake45.com">
+    Made with <img id="madjake45" src="http://madjake45.com/heart.png" alt="Heart" title="Love"> and <img id="madjake45" src="http://madjake45.com/turtle.png" alt="madjake45" title="Turtles"></a>
+</footer>
+</html>
